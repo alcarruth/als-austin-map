7da2e2153543763fa7abd93f3a23970a34cf33b8
Al Carruth <carruth@volente.com>
2016-05-02 20:41:56 -0500

    Added ISC license to source files.  Changes to be committed: 	modified:   src/css/style.css 	modified:   src/js/austin_places.coffee 	modified:   src/js/jsonp.coffee 	modified:   src/js/neighborhood_map.coffee 	modified:   src/js/wikipedia_api.coffee 	new file:   src/templates/license.coffee 	new file:   src/templates/license.css 	new file:   src/templates/license.html 	new file:   src/templates/license.txt 	new file:   src/templates/license_template.txt 	modified:   src/tools/build-nodes/index.coffee 	modified:   src/tools/build-nodes/index.js

--- src/tools/build-nodes/index.js
+++ src/tools/build-nodes/index.js
@@ -5,6 +5,26 @@
 
 /* build-nodes/index.coffee */
 
+
+/*
+ *  ISC License (ISC)
+ *  Copyright (c) 2016, Al Carruth <al.carruth@gmail.com>
+ * 
+ *  Permission to use, copy, modify, and/or distribute this software for
+ *  any purpose with or without fee is hereby granted, provided that the
+ *  above copyright notice and this permission notice appear in all
+ *  copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ *  WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ *  AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ *  CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ *  OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ *  NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ *  CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 (function() {
   var Apply, Clean_CSS, CoffeeScript, HTML_Minified_Page, HTML_Page, HTML_Template, JavaScript, QueryURL, SVG, Source, StyleSheet, beautify, beautify_comments, coffee, fs, htmlmin, js_beautify, minify, mustache, render, uglify_js,
     bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
